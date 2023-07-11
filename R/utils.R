e75758f8aabc5e077804346745015c1085546915
rogilmore <rog1@psu.edu>
2023-07-11 06:16:05 -0400

    Update & render

---
+++ R/utils.R
@@ -0,0 +1,10 @@
+# R/utils.R
+
+find_str_in_file <- function(fn, targ_str) {
+  stopifnot(is.character(targ_str))
+  stopifnot(is.character(fn))
+  stopifnot(file.exists(fn))
+  
+  message("In: '", fn, "'")
+  system(paste0("grep ", targ_str, " ", fn))
+}
