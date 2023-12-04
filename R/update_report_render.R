1cf9e534dd197116233ea6ffd2e403b7d674a4e9
rogilmore <rog1@psu.edu>
2023-12-04 07:29:20 -0500

    Update data & render report; tweak update_report_render().

--- R/update_report_render.R
+++ R/update_report_render.R
@@ -3,15 +3,25 @@
 update_report_render <- function(src_dir = 'src',
                                  open_rpt = TRUE,
                                  rpt_URL = 'docs/index.html') {
-  stopifnot(is.character(src_dir))
-  stopifnot(dir.exists(src_dir))
-  stopifnot(is.logical(open_rpt))
-  stopifnot(is.character(rpt_URL))
-  stopifnot(file.exists(rpt_URL))
+  # stopifnot(is.character(src_dir))
+  # stopifnot(dir.exists(src_dir))
+  # stopifnot(is.logical(open_rpt))
+  # stopifnot(is.character(rpt_URL))
+  # stopifnot(file.exists(rpt_URL))
   
+  assertthat::is.string(src_dir)
+  assertthat::is.dir(src_dir)
+  
+  assertthat::assert_that(is.logical(open_rpt))
+  
+  assertthat::is.string(rpt_URL)
+  assertthat::is.readable(rpt_URL)
+  
+  message("\n\n-------Updating data-------")
   library(targets)
   tar_make()
   
+  message("\n\n-------Rendering report-------")
   bookdown::render_book(src_dir)
   
   if (open_rpt)
