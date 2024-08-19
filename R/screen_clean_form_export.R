fcd2df71f8b819da36c6055f98c06f1f7381b2f3
rogilmore <rog1@psu.edu>
2024-08-19 07:19:10 -0400

    Data dictionary work.

--- R/screen_clean_form_export.R
+++ R/screen_clean_form_export.R
@@ -17,13 +17,41 @@ screen_clean_form_export <-
     
     # Delete non-data rows
     type_not_data <-
-      c('begin_group', 'note', 'end_group', 'acknowledge')
+      c('start', 
+        'end', 
+        'begin_group', 
+        'note', 
+        'end_group', 
+        'acknowledge')
+    
+    name_not_data <-
+      c('c_today', 
+        'check_childage', 
+        'check_childage_months', 
+        'check_childage_weeks',
+        'confirm_child_birthdate', 
+        'endscreener_preterm1', 
+        'endscreener_preterm3',
+        'endscreener_preterm2', 
+        'endscreener_birthweight',
+        'endscreener_birth_newborn_comp',
+        'endscreener_hearing',
+        'endscreener_vision',
+        'endscreener_illnesses_injuries',
+        'endscreener_momage',
+        'endscreener_biodad_age',
+        'endscreener_biomom_age')
     
     # Clean labels
    df <- df |>
      dplyr::mutate(label = stringr::str_remove(label, "^[A-Z]{1}[0-9]?[a-z]?\\.")) |>
      dplyr::mutate(label = stringr::str_remove_all(label, '["]')) |>
      dplyr::mutate(label = stringr::str_remove(label, "^ "))
+   
+   # Remove non-data
+   df <- df |>
+     dplyr::filter(!(type %in% type_not_data)) |>
+     dplyr::filter(!(name %in% name_not_data))
 
     # Export
     out_fn <-
@@ -33,17 +61,13 @@ screen_clean_form_export <-
       message("File exists: ", out_fn)
       if (overwrite) {
         message("Overwriting file: ", out_fn)
-        df <- df |>
-          dplyr::filter(!(type %in% type_not_data)) |>
-          readr::write_csv(out_fn)
+        readr::write_csv(df, out_fn)
       } else {
         message("File not overwritten.")
         return(NULL)
       }
     } else {
-      df |>
-        dplyr::filter(!(type %in% type_not_data)) |>
-        readr::write_csv(out_fn)
+      readr::write_csv(df, out_fn)
     }
     
     out_fn
--- R/screen_remove_metadata_fields.R
+++ R/screen_remove_metadata_fields.R
@@ -5,7 +5,18 @@ screen_remove_metadata_fields <- function(df) {
   
   df |>
     dplyr::select(
-      -dplyr::contains('note'),-dplyr::contains('instructions'),-dplyr::contains('acknowledge'),-dplyr::contains('screener'),-dplyr::contains('__'),-dplyr::contains('meta/instanceID'),-dplyr::contains('_uuid'),-dplyr::contains('_submission_time'),-dplyr::contains('_index'),-dplyr::contains('_parent_index'),-dplyr::contains('_tags'),-dplyr::contains('_version_'),
+      -dplyr::contains('note'),
+      -dplyr::contains('instructions'),
+      -dplyr::contains('acknowledge'),
+      -dplyr::contains('screener'),
+      -dplyr::contains('__'),
+      -dplyr::contains('meta/instanceID'),
+      -dplyr::contains('_uuid'),
+      -dplyr::contains('_submission_time'),
+      -dplyr::contains('_index'),
+      -dplyr::contains('_parent_index'),
+      -dplyr::contains('_tags'),
+      -dplyr::contains('_version_'),
       # used to compute guid-dplyr::contains('day'),-dplyr::contains('concat1'),-dplyr::contains('Participant_ID_concat2'),
       # other meta-`_id`,-start,-end,-update_date,
       # Spanish-language version-dplyr::contains('NOTA')
